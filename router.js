d87cfd8a27ff0e26c24eea89c5fd5fac878aaaa5
Rodrigo Paixao <rodrigopaixao19@gmail.com>
2020-07-04 16:35:18 -0300

    last routes were added

--- router.js
+++ router.js
@@ -9,12 +9,16 @@ router.get('/', userController.home);
 router.post('/register', userController.register);
 router.post('/login', userController.login); 
 router.post('/logout', userController.logout);
+router.post('/doesUsernameExist', userController.doesUsernameExist);
+router.post('/doesEmailExist', userController.doesEmailExist);
+
 /*********** User related routes ***********/
 
 /*********** Profile related routes ***********/
 router.get('/profile/:username', userController.ifUserExists, userController.sharedProfileData, userController.profilePostsScreen);
 router.get('/profile/:username/followers', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowersScreen);
 router.get('/profile/:username/following', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowingScreen);
+
 /*********** Profile related routes ***********/
 
 
