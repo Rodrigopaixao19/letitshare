d4a6adfaa8d5c5009383b463be147b7ef706cdb6
Rodrigo Paixao <rodrigopaixao19@gmail.com>
2020-07-04 16:33:44 -0300

    sanitization, csrf, and api creation

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-
 const express = require('express');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const flash = require('connect-flash');
 const markdown = require('marked');
+const csrf = require('csurf');
 const app = express();
 const sanitizeHTML = require('sanitize-html');
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/api', require('./router-api'));
 
 let sessionOptions = session({
     secret: 'Javascript is so cool',
@@ -37,14 +43,49 @@ app.use((req, res, next) => {
 })
 
 const router = require('./router');
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
  
 app.use(express.static('public'));
 app.set('views', 'views');
 app.set('view engine', 'ejs');
 
+app.use(csrf());
+
+app.use((req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+})
+
 app.use('/', router);
 
-module.exports = app;
+app.use((err, req, res, next) => {
+    if(err) {
+        if(err.code ==  'EBADCSRFTOKEN') {
+            req.flash('errors', 'Cross site request forgery detected');
+            req.session.save(() => res.redirect('/'))
+        }
+        else {
+            res.render('404')
+        }
+    }
+})
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.use((socket, next) => {
+    sessionOptions(socket.request, socket.request.res, next)
+})
+
+io.on('connection', (socket) => {
+    if(socket.request.session.user) {
+        let user = socket.request.session.user
+
+        socket.emit('welcome', {username: user.username, avatar: user.avatar})
+
+        socket.on('chatMessageFromBrowser', (data) => {
+        socket.broadcast.emit('chatMessageFromServer', {message: sanitizeHTML(data.message, {allowedTags: [], allowedAttributes:{}}), username: user.username, avatar: user.avatar})
+      })
+    }
+})
+
+module.exports = server;
