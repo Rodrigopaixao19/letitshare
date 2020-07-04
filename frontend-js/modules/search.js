e84b359585129cda2b6f9f4e2b8d938843eb18d8
Rodrigo Paixao <rodrigopaixao19@gmail.com>
2020-07-04 16:31:38 -0300

    chat and search added

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,15 +1,25 @@
+import axios from 'axios';
+import DOMPurify from 'dompurify';
+
 export default class Search {
     // select DOM elements, and keep track of any useful data
     constructor() {
+        this._csrf = document.querySelector('[name="_csrf"]').value;
         this.injectHTML();
         this.headerSearchIcon = document.querySelector('.header-search-icon');
         this.overlay = document.querySelector('.search-overlay');
         this.closeIcon = document.querySelector('.close-live-search');
+        this.inputField = document.querySelector('#live-search-field');
+        this.resultsArea = document.querySelector('.live-search-results');
+        this.loaderIcon = document.querySelector('.circle-loader');
+        this.typingWaitTimer;
+        this.previousValue = '';
         this.events();
     }
 
     // events
     events() {
+        this.inputField.addEventListener('keyup', () => this.keyPressHandler())
         this.closeIcon.addEventListener('click', () => this.closeOverlay());
         this.headerSearchIcon.addEventListener('click', (e) => {
             e.preventDefault();
@@ -20,10 +30,88 @@ export default class Search {
     // Methods
     openOverlay() {
         this.overlay.classList.add('search-overlay--visible');
+        setTimeout(() => this.inputField.focus(), 50)
     }
 
     closeOverlay() {
         this.overlay.classList.remove('search-overlay--visible')
+    }
+
+    keyPressHandler() {
+      let value = this.inputField.value;
+
+      if(value == '') {
+        clearTimeout(this.typingWaitTimer);
+        this.hideLoaderIcon();
+        this.hideResultsArea();
+      }
+      
+      if(value !== '' && value !== this.previousValue) {
+        clearTimeout(this.typingWaitTimer);
+        this.showLoaderIcon();
+        this.hideResultsArea();
+        this.typingWaitTimer = setTimeout(() => this.sendRequest(), 750);
+      }
+
+      this.previousValue = value;
+    }
+
+    sendRequest() {
+      axios.post('/search', {_csrf: this._csrf, searchTerm: this.inputField.value})
+      .then(response => {
+        this.renderResultsHTML(response.data)
+      })
+      .catch(() => {
+        alert('request failed')
+      })
+    }
+
+    renderResultsHTML(posts) {
+      if(posts.length) {
+        this.resultsArea.innerHTML = DOMPurify.sanitize(`
+        
+        <div class="list-group shadow-sm">
+        <div class="list-group-item active"><strong>Search Results</strong> (${posts.length > 1 ? `${posts.length} items found` : `1 item found`})</div>
+         ${posts.map(post => {
+           let postDate = new Date(post.createdDate)
+           return `
+           <a href="/post/${post._id}" class="list-group-item list-group-item-action">
+            <img class="avatar-tiny" src="${post.author.avatar}"> <strong>${post.title}</strong>
+            <span class="text-muted small">by ${post.author.username} on ${postDate.getMonth() + 1}/${postDate.getDate()}/${postDate.getFullYear()}</span>
+             </a>
+           `
+         }).join('')}     
+        </div>
+        ;
+        `)
+        
+      } 
+      else {
+        this.resultsArea.innerHTML = 
+        `
+        <p class="alert alert-danger text-center shadow-sm">Sorry, we could not find any results for that search</p>
+        `;
+      }
+
+      this.hideLoaderIcon();
+      this.showResultsArea();
+    }
+
+
+    showLoaderIcon() {
+      this.loaderIcon.classList.add('circle-loader--visible')
+    }
+
+    hideLoaderIcon() {
+      this.loaderIcon.classList.remove('circle-loader--visible')
+    }
+
+    showResultsArea() {
+      this.resultsArea.classList.add('live-search-results--visible')
+    }
+
+    hideResultsArea() {
+      this.resultsArea.classList.remove('live-search-results--visible')
     }
 
     injectHTML() {
@@ -41,28 +129,7 @@ export default class Search {
           <div class="search-overlay-bottom">
             <div class="container container--narrow py-3">
               <div class="circle-loader"></div>
-              <div class="live-search-results live-search-results--visible">
-                <div class="list-group shadow-sm">
-                  <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
-      
-                  <a href="#" class="list-group-item list-group-item-action">
-                    <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #1</strong>
-                    <span class="text-muted small">by barksalot on 0/14/2019</span>
-                  </a>
-                  <a href="#" class="list-group-item list-group-item-action">
-                    <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #2</strong>
-                    <span class="text-muted small">by brad on 0/12/2019</span>
-                  </a>
-                  <a href="#" class="list-group-item list-group-item-action">
-                    <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #3</strong>
-                    <span class="text-muted small">by barksalot on 0/14/2019</span>
-                  </a>
-                  <a href="#" class="list-group-item list-group-item-action">
-                    <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #4</strong>
-                    <span class="text-muted small">by brad on 0/12/2019</span>
-                  </a>
-                </div>
-              </div>
+              <div class="live-search-results"></div>
             </div>
           </div>
         </div>
